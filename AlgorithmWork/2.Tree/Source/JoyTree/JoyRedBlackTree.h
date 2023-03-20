b904a4b95f7d21d18f121541e2b2add3f1f13bdc
DexuanYang <joyat.yang@igg.com>
2023-03-20 19:37:42 +0800

    * 红黑树函数声明

--- AlgorithmWork/2.Tree/Source/JoyTree/JoyRedBlackTree.h
+++ AlgorithmWork/2.Tree/Source/JoyTree/JoyRedBlackTree.h
@@ -21,7 +21,7 @@ namespace Joy
 	{
 	public:
 		/// <summary>
-		/// 红黑节点枚举定义
+		/// 红黑树节点颜色枚举定义
 		/// </summary>
 		enum class EnumColor : unsigned char
 		{
@@ -38,15 +38,124 @@ namespace Joy
 			T elementData;
 			RedBlackNode* pLeftChild{ nullptr };
 			RedBlackNode* pRightChild{ nullptr };
-			EnumColor colorFlag{ EnumColor::Black };
+			EnumColor colorTag{ EnumColor::Black };
 
 			RedBlackNode(const T& element, RedBlackNode* pLeftNode = nullptr, RedBlackNode* pRightNode = nullptr, EnumColor color = EnumColor::BLACK)
-				: elementData(element), pLeftChild(pLeftNode), pRightChild(pRightChild), colorFlag(color)
+				: elementData(element), pLeftChild(pLeftNode), pRightChild(pRightChild), colorTag(color)
 			{
 			}
 		};
 
+	public:
+		JoyRedBlackTree()
+			: m_Root(nullptr)
+		{
+		}
+
+		JoyRedBlackTree(const JoyRedBlackTree& rhs)
+		{
+		}
+
+		explicit JoyRedBlackTree(const T& element)
+			: m_Root(new RedBlackNode(element, nullptr, nullptr, EnumColor::BLACK))
+		{
+		}
+
+		~JoyRedBlackTree()
+		{
+		}
+
+	public:
+		void PrintTree() const
+		{
+			std::cout << "PreOrder  ================" << std::endl;
+			PrintTreePreOrder(m_Root);
+			std::cout << "InOrder   ================" << std::endl;
+			PrintTreeInOrder(m_Root);
+			std::cout << "PostOrder ================" << std::endl;
+			PrintTreePostOrder(m_Root);
+		}
+
+		void MakeEmpty()
+		{
+		}
+
+		/// <summary>
+		/// 向红黑树新增一个元素节点
+		/// </summary>
+		/// <param name="element"></param>
+		void Insert(const T& element)
+		{
+		}
+
+		/// <summary>
+		/// 从红黑树删除指定的元素节点
+		/// </summary>
+		/// <param name="element"></param>
+		void Remove(const T& element)
+		{
+		}
+	
 	private:
+		/// <summary>
+		/// 前序遍历打印树
+		/// </summary>
+		/// <param name="pNode"></param>
+		void PrintTreePreOrder(RedBlackNode* pNode)
+		{
+
+		}
+		/// <summary>
+		/// 中序遍历打印树
+		/// </summary>
+		/// <param name="pNode"></param>
+		void PrintTreeInOrder(RedBlackNode* pNode)
+		{
+		}
+		/// <summary>
+		/// 后序遍历打印树
+		/// </summary>
+		/// <param name="pNode"></param>
+		void PrintTreePostOrder(RedBlackNode* pNode)
+		{
+		}
+
+		/// <summary>
+		/// 清空树
+		/// </summary>
+		/// <param name="pNode"></param>
+		void MakeEmpty(RedBlackNode* pNode)
+		{
+		}
+
+		/// <summary>
+		/// 向红黑树插入一个元素，并维持红黑树平衡
+		/// </summary>
+		/// <param name="element"></param>
+		/// <param name="pNode"></param>
+		void Insert(const T& element, RedBlackNode*& pNode)
+		{
+			if (pNode == nullptr)
+			{
+				pNode = new RedBlackNode(element, nullptr, nullptr, EnumColor::RED);
+			}
+		}
+
+		/// <summary>
+		/// 删除红黑树的一个元素节点，并维持红黑树平衡
+		/// </summary>
+		/// <param name="element"></param>
+		/// <param name="pNode"></param>
+		void Remove(const T& element, RedBlackNode*& pNode)
+		{
+			// 不存在要删除的元素节点，直接返回，不处理
+			if (pNode == nullptr) { return; }
+		}
+
+	private:
+		/// <summary>
+		/// 红黑树根节点
+		/// </summary>
 		RedBlackNode* m_Root{ nullptr };
 	};
 }
