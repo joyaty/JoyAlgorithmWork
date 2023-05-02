3d4d1398846edf86fdd0bfacf364ad4a1ac93ba1
joyaty <joyaty@gmail.com>
2023-05-02 13:00:22 +0800

    * Test Cpp IO Library

---
+++ LanguageWork/1.IOLibrary/Source/CppIOLibrary/CppIOLibraryWork.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+/// <summary>
+/// C++ IO标准库练习
+/// </summary>
+void TestCppIOLibraryWork()
+{
+	std::cout << "This is Cpp IO Library===============" << std::endl;
+
+	std::ofstream fout1;
+
+
+	std::cout << "=====================================" << std::endl;
+}
--- LanguageWork/1.IOLibrary/Source/Main.cpp
+++ LanguageWork/1.IOLibrary/Source/Main.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
 
+void TestCppIOLibraryWork();
+
 int main()
 {
 	std::cout<< "Hello IO Library" << std::endl;
+	TestCppIOLibraryWork();
 	std::cin.get();
 	return 0;
 }
