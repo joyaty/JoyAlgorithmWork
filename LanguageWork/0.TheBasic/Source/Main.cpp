8397bf1529fe115778bd3c698a3822ec79fd2145
Joyaty <joyaty@gmail.com>
2023-08-07 21:04:07 +0800

    * LanguageWork/0.TheBasic: Language basic test.

--- LanguageWork/0.TheBasic/Source/Main.cpp
+++ LanguageWork/0.TheBasic/Source/Main.cpp
@@ -1,9 +1,190 @@
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+
+class ClassTemp
+{
+public:
+	ClassTemp() = default;
+	ClassTemp(const char* str)
+		: m_strMember(str)
+	{
+	}
+	ClassTemp(const ClassTemp& temp)
+		: m_strMember(temp.m_strMember)
+	{
+		std::cout << "Copy Constractor" << std::endl;
+	}
+
+	ClassTemp(ClassTemp&& temp) noexcept
+		: m_strMember(temp.m_strMember)
+	{
+		std::cout << "Move Constractor" << std::endl;
+	}
+
+	void TestThrowException()
+	{
+		throw std::runtime_error("Test Runtime Error!");
+	}
+
+public:
+	std::string m_strMember;
+	int m_iMember;
+};
+
+void PrintArray(const int (*arr)[5])
+{
+	for (int value : *arr)
+	{
+		std::cout << value << std::endl;
+	}
+}
+
+void PopBackArray(int(*arr)[5])
+{
+	int arrbackup[] = { (*arr)[4], (*arr)[3], (*arr)[2], (*arr)[1], (*arr)[0] };
+
+	int i = 0;
+	for (const int& value : arrbackup)
+	{
+		(*arr)[i] = value;
+		++i;
+	}
+
+	PrintArray(arr);
+
+}
+
+static int s_ArrayInts[] = { 5, 4, 3, 2, 1 };
+
+int (*GetArray1())[5]
+{
+	return &s_ArrayInts;
+}
+
+auto GetArray2() -> int(*)[5]
+{
+	return &s_ArrayInts;
+}
+
+using ArrayInt = int[5];
+ArrayInt* GetArray3()
+{
+	return &s_ArrayInts;
+}
+
+decltype(s_ArrayInts)* GetArray4()
+{
+	return &s_ArrayInts;
+}
+
+constexpr int GetArraySize(int iScale)
+{
+	return 5 * iScale;
+}
+
+inline bool CompareString(const std::string& lstr, const std::string& rstr)
+{
+	return lstr.size() <= rstr.size();
+}
+
+typedef bool Func1(const std::string&, const std::string&);
+using Func2 = bool(const std::string&, const std::string&);
+
+typedef bool (*FuncPtr1)(const std::string&, const std::string&);
+using FuncPtr2 = bool(*)(const std::string&, const std::string&);
+
+const std::string& UseBigger(const std::string& lstr, const std::string& rstr, FuncPtr2 func)
+{
+	return func(lstr, rstr) ? rstr : lstr;
+}
+
+Func1* GetFunc()
+{
+	return CompareString;
+}
+FuncPtr2 GetFunc2()
+{
+	return CompareString;
+}
+
+bool (*GetFunc3())(const std::string&, const std::string&)
+{
+	return CompareString;
+}
+
+auto GetFunc4() -> bool(*)(const std::string&, const std::string&)
+{
+	return CompareString;
+}
+
+auto GetFunc5() -> Func2*
+{
+	return CompareString;
+}
+
+auto GetFunc6() -> FuncPtr1
+{
+	return CompareString;
+}
 
 int main()
 {
 	std::cout << "Hello, The Basic" << std::endl;
+
+	int size1 = sizeof(ClassTemp);
+	ClassTemp pClassTmp("123412342134");
+
+	int i = 108, j = 5;
+	float r = static_cast<float>(j) / i;
+	void* p = &i;
+	int* pi = static_cast<int*>(p);
+	char* pc = reinterpret_cast<char*>(pi);
+	std::string stri = pc;
+	constexpr int k = 10;
+	const int* cpk = &k;
+	int* pk = const_cast<int*>(cpk);
+	*pk = 50;
+
+	std::cout << stri << std::endl;
+
+	bool bSwitchFlag = false;
+	switch (bSwitchFlag)
+	{
+	case true:
+		int value;
+		break;
+	case false:
+		value = 10;
+		std::cout << "value = " << value << std::endl;
+		break;
+	}
+
+	try
+	{
+		pClassTmp.TestThrowException();
+	}
+	catch (const std::runtime_error& error)
+	{
+		std::cout << error.what() << std::endl;
+	}
+
+	PopBackArray(GetArray1());
+	PopBackArray(GetArray2());
+	PopBackArray(GetArray3());
+	PopBackArray(GetArray4());
+
+	std::cout << "=============================" << std::endl;
+	int arrayInts1[GetArraySize(2)];
+	int iValue = 1;
+	int sizeValue = GetArraySize(iValue);
+	std::cout << sizeValue << std::endl;
+
+	std::cout << "=============================" << std::endl;
+	Func1* func = GetFunc();
+	std::cout << UseBigger("Hello", " World!", func);
+
 	std::cin.get();
 	return 0;
 }
